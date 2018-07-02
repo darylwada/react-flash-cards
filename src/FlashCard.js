f37fa0d6f12afb89f5ab9d6bc1180e89bc605c61
Daryl Wada <daryl.wada@gmail.com>
2018-07-02 15:43:08 -0700

    Consolidated input components into one that takes the differences as props. #1

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import QuestionInput from './QuestionInput'
-import AnswerInput from './AnswerInput'
+import Input from './Input'
 
 export default class FlashCard extends Component {
   constructor(props) {
@@ -28,8 +27,8 @@ export default class FlashCard extends Component {
         className="card border col-6 mx-auto d-flex align-self-center p-3"
         onSubmit={this.handleSubmit}>
         <h5 className="card-title text-center">Create a Flash Card</h5>
-        <QuestionInput />
-        <AnswerInput />
+        <Input label="Question" placeholder="Example: What is 2 + 2?" name="question" />
+        <Input label="Answer" placeholder ="Example: 4" name="answer" />
         <button type="submit" className="btn btn-primary mx-auto my-3">Save</button>
       </form>
     )
