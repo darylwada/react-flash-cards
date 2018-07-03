8c37484dbdd1e68bd82788655ff56e1f7a878578
Daryl Wada <daryl.wada@gmail.com>
2018-07-03 10:37:04 -0700

    Styled components so views are centered on the screen. #3

--- src/Cards.js
+++ src/Cards.js
@@ -2,15 +2,19 @@ import React from 'react'
 
 export default function Cards(props) {
   const cardList = props.currentCards.length > 0
-    ? props.currentCards.map(card =>
-        <li className="list-group-item" key={card.question}>
-          <p>{card.question}</p>
-          <p>{card.answer}</p>
-        </li>
-      )
-    : <div className="card">
-        <h5 className="card-title">You have no flash cards.</h5>
-        <a className="mx-auto my-3" href={'#new-card'}>
+    ? <ul className="list-group">
+        {
+          props.currentCards.map(card =>
+            <li className="list-group-item" key={card.question}>
+              <p>{card.question}</p>
+              <p>{card.answer}</p>
+            </li>
+          )
+        }
+      </ul>
+    : <div className="card border-0 w-100">
+        <h5 className="card-title text-center">You have no flash cards.</h5>
+        <a className="mx-auto my-5" href={'#new-card'}>
           <button className="btn btn-primary">Make One</button>
         </a>
       </div>
@@ -18,9 +22,7 @@ export default function Cards(props) {
 
   return (
     <div className="col-6 mx-auto d-flex align-self-center p-3">
-      <ul className="list-group">
-        {cardList}
-      </ul>
+      {cardList}
     </div>
   )
 }
