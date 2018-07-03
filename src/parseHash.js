0b90631d6399f5b2ecb47be775aa574a954e7ac0
Daryl Wada <daryl.wada@gmail.com>
2018-07-03 11:09:58 -0700

    Removed unnecessary query string files. #3

--- src/parseHash.js
+++
@@ -1,8 +0,0 @@
-import * as queryString from './queryString'
-
-export function parseHash(hash) {
-  const split = hash.split('?')
-  const path = split[0].slice(1)
-  const params = queryString.parse(split[1])
-  return { path, params }
-}
