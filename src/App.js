65bc3f6efccf19304406065f90be7a3a3e02038b
Daryl Wada <daryl.wada@gmail.com>
2018-07-03 10:44:20 -0700

    Cards are now stored in local storage. #3

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      path: 'cards',
-      cards: []
+      path: localStorage.path,
+      cards: JSON.parse(localStorage.cards)
     }
     this.updateCardList = this.updateCardList.bind(this)
   }
@@ -21,6 +21,12 @@ export default class App extends Component {
     })
 
     window.dispatchEvent(new Event('hashchange'))
+
+    window.addEventListener('beforeunload', () => {
+      for (let key in this.state) {
+        localStorage.setItem(key, JSON.stringify(this.state[key]))
+      }
+    })
   }
 
   updateCardList(cards) {
@@ -36,12 +42,14 @@ export default class App extends Component {
   }
 
   renderView() {
+    console.log(this.state)
+    console.log(localStorage)
     if (this.state.path === 'new-card') return this.renderNewCard()
     if (this.state.path === 'cards') return this.renderCards()
   }
 
   render() {
-    console.log(this.state)
+
     return (
       <div className="container d-flex h-100">
         <Nav path={this.state.path}/>
