528eaec63d102a9b28396675546f97e2dfe4169f
Daryl Wada <daryl.wada@gmail.com>
2018-07-04 10:43:42 -0700

    Added an edit icon to the cards and added a query string to the links. #7

--- src/CardList.js
+++ src/CardList.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import * as queryString from './query-string'
 
-export default function Cards({ currentCards }) {
+export default function Cards({ currentCards, params }) {
   return (
     <ul className="list-group card border-0 w-100">
       {
@@ -8,6 +9,7 @@ export default function Cards({ currentCards }) {
           <li className="list-group-item" key={index}>
             <h5 className="card-title">{card.question}</h5>
             <p className="card-body">{card.answer}</p>
+            <a href={'#cards' + queryString.stringify({ 'cardId': index + 1 })}><i className="fas fa-edit"></i></a>
           </li>
         )
       }
