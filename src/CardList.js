0036d01228d46ca937a4b08a6a5fb55ca6f46968
Daryl Wada <daryl.wada@gmail.com>
2018-07-07 19:52:14 -0700

    Added a slide out animation to a deleted card. #15

--- src/CardList.js
+++ src/CardList.js
@@ -4,13 +4,16 @@ import * as queryString from './query-string'
 export default class CardList extends Component {
   constructor(props) {
     super(props)
+    this.state = { transition: null, deleteIndex: null }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick({ target }) {
     const { deleteCard } = this.props
     const deleteIndex = parseInt(target.getAttribute('data-index'), 10)
-    deleteCard(deleteIndex)
+    this.setState({ transition: 'deleting', deleteIndex })
+    setTimeout(() => deleteCard(deleteIndex), 500)
+    setTimeout(() => this.setState({ deleteIndex: null }), 500)
   }
 
   render() {
@@ -20,8 +23,11 @@ export default class CardList extends Component {
         {
           currentCards.map((card, index) => {
             const query = queryString.stringify({ 'cardIdx': index + 1 })
+            const deleteClass = index === this.state.deleteIndex
+              ? ' slideOutLeft'
+              : ''
             return (
-              <li className="list-group-item mb-3 shadow-sm" key={index}>
+              <li className={'list-group-item mb-3 shadow-sm' + deleteClass} key={index}>
                 <h5 className="card-title">{card.question}</h5>
                 <p className="card-body m-0 py-0">{card.answer}</p>
                 <div className="row no-gutters justify-content-end">
