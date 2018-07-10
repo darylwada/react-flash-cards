ec807adc57d9380401d09083e9e65593837f5bb0
Daryl Wada <daryl.wada@gmail.com>
2018-07-09 21:08:09 -0700

    Refactored delete animations so that they rely on transition end and made transform dynamic with the size of the card. #15

--- src/CardList.js
+++ src/CardList.js
@@ -4,43 +4,65 @@ import * as queryString from './query-string'
 export default class CardList extends Component {
   constructor(props) {
     super(props)
-    this.state = { transition: null, deleteIndex: null, offset: [] }
+    this.state = { transition: null, deleteIndex: null, heights: [] }
     this.handleClick = this.handleClick.bind(this)
+    this.handleTransitionEnd = this.handleTransitionEnd.bind(this)
   }
 
   handleClick({ target }) {
-    const { deleteCard } = this.props
     const deleteIndex = parseInt(target.getAttribute('data-index'), 10)
     this.setState({ transition: 'deleting', deleteIndex })
-    setTimeout(() => deleteCard(deleteIndex), 500)
-    setTimeout(() => this.setState({ transition: null }), 500)
+  }
+
+  handleTransitionEnd() {
+    const { deleteCard } = this.props
+    deleteCard(this.state.deleteIndex)
+    const heights = this.state.heights
+    heights.splice(this.state.deleteIndex, 1)
+    this.setState({ transition: null, heights })
   }
 
   componentDidMount() {
-    const offset = []
+    const heights = []
     for (const key in this.refs) {
-      offset.push(this.refs[key].offsetTop)
+      heights.push(this.refs[key].clientHeight)
     }
-    this.setState({ offset })
+    this.setState({ heights })
     console.log(this.refs)
   }
 
   render() {
     console.log(this.state)
     const { currentCards } = this.props
+    const { deleteIndex, transition } = this.state
     return (
       <ul className="list-group fixed-width-700 mx-auto">
         {
           currentCards.map((card, index) => {
             const query = queryString.stringify({ 'cardIdx': index + 1 })
-            const deleteClass = index === this.state.deleteIndex && this.state.transition
-              ? ' slideOutLeft'
-              : ''
-            const slideClass = index >= this.state.deleteIndex && !this.state.transition && this.state.deleteIndex !== null
-              ? ' slideUp'
-              : ''
+
+            let deleteClass = ''
+            let remainingClass = ''
+            let handleMethod = null
+            let slideObj = null
+            if (index === deleteIndex && transition) {
+              deleteClass = ' slide slideOutLeft'
+              handleMethod = this.handleTransitionEnd
+            }
+            else if (transition) {
+              remainingClass = ' slide'
+            }
+            if (index > deleteIndex && transition && deleteIndex !== null) {
+              slideObj = { transform: `translateY(${-1 * this.state.heights[deleteIndex] - 18}px)` }
+            }
+
             return (
-              <li ref={'card-' + index} className={'list-group-item mb-3 shadow-sm' + deleteClass + slideClass} key={index}>
+              <li 
+                ref={'card-' + index} 
+                className={'list-group-item mb-3 shadow-sm' + deleteClass + remainingClass} 
+                style={slideObj}
+                onTransitionEnd={handleMethod} 
+                key={index}>
                 <h5 className="card-title">{card.question}</h5>
                 <p className="card-body m-0 py-0">{card.answer}</p>
                 <div className="row no-gutters justify-content-end">
